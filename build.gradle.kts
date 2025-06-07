dd349255b01622e3130348c67b370454c287cedb
Sebastian Nowak <sebnow91@gmail.com>
2025-06-07 22:47:18 +0200

    8: Adjust workflow to run faster and add ktlint to kotlin plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.8"
+    id("org.jlleitschuh.gradle.ktlint") version "12.3.0"
 }
