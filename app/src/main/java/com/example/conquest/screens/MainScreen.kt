8b52d3aadfdbdd126f93f832e588f7b36d37de4c
Sebastian Nowak <sebnow91@gmail.com>
2025-07-14 12:49:02 +0200

    5: add intial display of the list of cosplays, improve styling of new cosplay screen

--- app/src/main/java/com/example/conquest/screens/MainScreen.kt
+++ app/src/main/java/com/example/conquest/screens/MainScreen.kt
@@ -1,54 +1,62 @@
 package com.example.conquest.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.example.conquest.CosplayViewModel
+import kotlinx.serialization.Serializable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import kotlinx.serialization.Serializable
+import androidx.compose.ui.text.font.FontWeight
 
 @Serializable
 object MainScreen
 
 @Composable
 fun MainScreen(navController: NavController) {
-    var text by remember {
-        mutableStateOf("")
-    }
+    val cosplayViewModel: CosplayViewModel = viewModel()
+    val cosplays by cosplayViewModel.allCosplays.collectAsState()
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(18.dp)
     ) {
-        TextField(value = text, onValueChange = {
-            text = it
-        },
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        Button(onClick = {
-            navController.navigate(SettingsScreenParams(
-                name = text,
-                age = 25
-            ))
-        },
-            modifier = Modifier.align(Alignment.End)) {
-            Text(text = "To Detail Screen")
+        itemsIndexed(cosplays) { index, cosplay ->
+            Card(
+                modifier = Modifier.fillMaxWidth()
+                    .border(
+                        width = 1.dp,
+                        color = MaterialTheme.colorScheme.outline,
+                        shape = RoundedCornerShape(24.dp)
+                    ),
+                colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background),
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+                shape = RoundedCornerShape(32.dp)
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Text(
+                        text = cosplay.name,
+                        style = MaterialTheme.typography.titleMedium,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
+            }
         }
-
     }
 }
